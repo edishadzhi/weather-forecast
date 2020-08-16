1e60a3a85940d303274f63c274db62b0f36529f6
edishadzhi <edishadzhi@gmail.com>
2020-08-16 10:55:44 +0300

    Updates

---
+++ precache-manifest.e3a4ba876f1de4784a465df5e1f4c3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920e00a7d5bb03c7649fe9efe373775e",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "4e7834cd1d0b885e8e02",
+    "url": "/weather-forecast/static/css/main.19923d5f.chunk.css"
+  },
+  {
+    "revision": "427783e7c31b706ce14c",
+    "url": "/weather-forecast/static/js/2.dc9bec3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-forecast/static/js/2.dc9bec3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7834cd1d0b885e8e02",
+    "url": "/weather-forecast/static/js/main.d19d9a9d.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  }
+]);
